5d64709bc5341deca983d5c04c25ad045264c684
wacom_tablet <wacom_tablet@163.com>
2017-12-25 13:51:32 +0800

    大法师打发斯蒂芬
    ...

---
+++ MomoForm/Momo.AutoUpgrade.Core/MFile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Momo.AutoUpgrade.Core
+{
+    /// <summary>
+    /// 升级文件信息
+    /// </summary>
+    [Serializable]
+    public sealed class MFile
+    {
+        /// <summary>
+        /// 文件名
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 相对路径，空表示根目录
+        /// </summary>
+        public string Path { get; set; }
+    }
+}
